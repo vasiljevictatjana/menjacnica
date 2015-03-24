cb4d6a004d29511392a2bc0f0cbab099bad17d93
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-24 18:31:29 +0100

    Dodate toString, hashCode i equals metode u klase Valuta i Kurs

--- src/menjacnica/Kurs.java
+++ src/menjacnica/Kurs.java
@@ -49,5 +49,60 @@ public class Kurs {
 	public void setValute(LinkedList<Valuta> valute) {
 		this.valute = valute;
 	}
+
+	@Override
+	public String toString() {
+		return "Kurs [prodajniKurs=" + prodajniKurs + ", kupovniKurs="
+				+ kupovniKurs + ", srednjiKurs=" + srednjiKurs
+				+ ", datumKursa=" + datumKursa + ", valute=" + valute + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((datumKursa == null) ? 0 : datumKursa.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datumKursa == null) {
+			if (other.datumKursa != null)
+				return false;
+		} else if (!datumKursa.equals(other.datumKursa))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double
+				.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double
+				.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double
+				.doubleToLongBits(other.srednjiKurs))
+			return false;
+		if (valute == null) {
+			if (other.valute != null)
+				return false;
+		} else if (!valute.equals(other.valute))
+			return false;
+		return true;
+	}
 	
 }
--- src/menjacnica/Valuta.java
+++ src/menjacnica/Valuta.java
@@ -19,5 +19,43 @@ public class Valuta {
 	public void setSkraceniNaziv(String skraceniNaziv) {
 		this.skraceniNaziv = skraceniNaziv;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result
+				+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ "]";
+	}
 	
 }
