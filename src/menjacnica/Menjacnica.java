c476178c6370743919f712f2a74fdf23b3f958f2
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-26 21:35:19 +0100

    Implementirane sve metode u klasi Menjacnica

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -6,12 +6,28 @@ import java.util.LinkedList;
 import interfejsmenjacnice.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi = 
+			new LinkedList<Kurs>();
 
 	public void dodajKurs(Kurs kurs) {
 		
+		if (kurs==null)
+			throw new RuntimeException("Kurs ne sme biti null!");
+		
+		if (kursevi.contains(kurs))
+			throw new RuntimeException("Kurs vec postoji!");
+		
+		kursevi.add(kurs);
+
 	}
 
 	public void obrisiKurs(Kurs kurs) {
+		
+		if (!kursevi.contains(kurs))
+			throw new RuntimeException("Kurs ne postoji!");
+		
+		kursevi.remove(kurs);
 		
 	}
 
@@ -19,7 +35,20 @@ public class Menjacnica implements MenjacnicaInterfejs {
 			double prodajniKurs, double kupovniKurs, double srednjiKurs,
 			Valuta valuta) {
 		
-		return null;
+		LinkedList<Kurs> pronadjeniKursevi = 
+				new LinkedList<Kurs>();
+		
+		if (datumKursa==null && prodajniKurs<=0 && kupovniKurs<=0 && srednjiKurs<=0)
+			throw new RuntimeException("Mora biti unet neki kriterijum!");
+		
+		if (!datumKursa.after(new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH, GregorianCalendar.DAY_OF_MONTH)))
+			for (int i=0;i<kursevi.size();i++)
+				if (kursevi.get(i).getDatumKursa()==datumKursa)
+					pronadjeniKursevi.add(kursevi.get(i));
+		
+
+		
+		return pronadjeniKursevi;
 	}
 
 }
