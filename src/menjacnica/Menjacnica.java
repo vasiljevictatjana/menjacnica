76b0f7c272a338f4a294950836dca4f4f0f6521c
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-26 21:26:10 +0100

    Napravljena klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import interfejsmenjacnice.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Kurs kurs) {
+		
+	}
+
+	public void obrisiKurs(Kurs kurs) {
+		
+	}
+
+	public LinkedList<Kurs> pronadjiKurs(GregorianCalendar datumKursa,
+			double prodajniKurs, double kupovniKurs, double srednjiKurs,
+			Valuta valuta) {
+		
+		return null;
+	}
+
+}
