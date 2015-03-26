fa32e543d165d7ce94233a0f4591f443181e22e0
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-26 21:38:48 +0100

    Implementirana metoda dodajKurs u okviru klase Menjacnica na master grani

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -6,9 +6,15 @@ import java.util.LinkedList;
 import interfejsmenjacnice.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi = 
+			new LinkedList<Kurs>();
 
 	public void dodajKurs(Kurs kurs) {
+		if(kurs.getDatumKursa().before(new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH, GregorianCalendar.DAY_OF_MONTH)))
+			throw new RuntimeException("Datum ne sme biti u proslosti!");
 		
+		kursevi.add(kurs);
 	}
 
 	public void obrisiKurs(Kurs kurs) {
